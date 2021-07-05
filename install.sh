461079922baad6f273f2cb8980ecf2e6439d9368
zinsking <35989723+zinsking@users.noreply.github.com>
2021-07-05 23:43:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,10 @@ cd nginx-1.16.1
 ./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-pcre=../pcre-8.43 --with-pcre-jit --with-zlib=../zlib-1.2.11 --with-openssl=../openssl-1.1.1d --with-openssl-opt=no-nextprotoneg --with-debug --add-module=../ngx_cache_purge --add-module=../ngx_http_substitutions_filter_module --add-module=../headers-more-nginx-module --add-dynamic-module=../ngx_http_google_filter_module
 sleep 1
 make && make install
+sleep 1
+mkdir /etc/nginx/modules -p
+cp objs/ngx_http_google_filter_module.so /etc/nginx/modules/
+chmod 777 /etc/nginx/modules/ngx_http_google_filter_module.so
 cd ~ 
 #创建软连接及目录
 ln -s /usr/lib/nginx/modules /etc/nginx/modules
@@ -27,8 +31,10 @@ mkdir /var/cache/nginx -p
 mkdir /etc/nginx/vhost -p
 #添加虚拟主机支持
 cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-wget -q -O nginx.conf https://raw.githubusercontent.com/zunsking/Debian-Install-Nginx/master/nginx.conf
+wget -q -O nginx.conf https://raw.githubusercontent.com/zinsking/ng-g/main/nginx.conf
 \cp nginx.conf /etc/nginx/
+wget -q -O vhost.conf https://raw.githubusercontent.com/zinsking/ng-g/main/vhost.conf
+cp vhost.conf /etc/nginx/vhost/
 #注册系统服务
 cat >/lib/systemd/system/nginx.service << EOF
 [Unit]
@@ -51,3 +57,4 @@ systemctl daemon-reload
 systemctl enable nginx.service
 systemctl start nginx.service
 systemctl status nginx.service
+service nginx restart
